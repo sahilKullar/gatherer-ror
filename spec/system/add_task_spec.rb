8ff02da0a62f3db17f560a485b8012e74547b143
Sahil Kullar <sahil_kullar@outlook.com>
2022-12-03 11:22:31 +0530

    integration test of javascript using capybara

--- spec/system/add_task_spec.rb
+++ spec/system/add_task_spec.rb
@@ -4,10 +4,37 @@ require "rails_helper"
 
 RSpec.describe "adding a new task" do
   let!(:project) { create(:project, name: "Project Bluebook") }
-  # let!(:task_1) { create(:task, project:, title: "Search Sky", size: 1) }
-  # let!(:task_2) { create(:task, project:, title: "Use Telescope", size: 1) }
   let!(:task_1) { create(:task, project:, title: "Search Sky", size: 1, project_order: 1) }
   let!(:task_2) { create(:task, project:, title: "Use Telescope", size: 1, project_order: 2) }
+  let!(:task_3) { create(:task, project:, title: "Take Notes", size: 1, project_order: 3) }
+
+  it "can add a task" do
+    visit(project_path(project))
+    fill_in("Task", with: "Find UFOs")
+    select("2", from: "Size")
+    click_on("Add Task")
+    expect(page).to have_current_path(project_path(project), ignore_query: true)
+
+    within("#task_4") do
+      expect(page).to have_selector(".name", text: "Find UFOs")
+      expect(page).to have_selector(".size", text: "2")
+    end
+  end
+
+  it "can re-order a task", :js do
+    visit(project_path(project))
+
+    within("#task_3") do
+      click_on("Up")
+    end
+
+    expect(page).to have_selector("tbody:nth-child(2) .name", text: "Take Notes")
+    visit(project_path(project))
+
+    within("#task_2") do
+      expect(page).to have_selector(".name", text: "Take Notes")
+    end
+  end
 
   it "can add and reorder a task" do
     visit(project_path(project))
